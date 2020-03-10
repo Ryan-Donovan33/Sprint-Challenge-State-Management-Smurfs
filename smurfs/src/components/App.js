cc64df729643255e08eb3916bd3a39899b40bca9
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-03-09 21:17:34 -0400

    Added component into App

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component, useState } from 'react';
 
 import Smurfs from './smurfs';
+import SmurfsForm from './smurfsForm';
 import './App.css';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
 			<h1>SMURFS! 2.0 W/ Redux</h1>
 			<div>Welcome to your state management version of Smurfs!</div>
 			<Smurfs />
+			<SmurfsForm />
 		</div>
 	);
 };
